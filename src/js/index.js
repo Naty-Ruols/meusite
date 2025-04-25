d4dcb512227a23be587f7e11558feb00df6b0dc8
Natalia Cerqueira <153464979+Naty-Ruols@users.noreply.github.com>
2025-04-24 22:23:29 -0300

    Update index.js

--- src/js/index.js
+++ src/js/index.js
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 const botaoMostrarProjetos = document.querySelector('.btn-mostrar-projetos');
 const projetosInativos = document.querySelectorAll('.projeto:not(.ativo)')
 
@@ -12,18 +12,4 @@ function newFunction() {
         projetoInativo.classList.add('ativo');
     });
 }
-=======
-const botaoMostrarProjetos = document.querySelector('.btn-mostrar-projetos');
-const projetosInativos = document.querySelectorAll('.projeto:not(.ativo)')
 
-botaoMostrarProjetos.addEventListener('click', () => {
-    newFunction();
-    botaoMostrarProjetos.classList.add("remover")
-})
-
-function newFunction() {
-    projetosInativos.forEach(projetoInativo => {
-        projetoInativo.classList.add('ativo');
-    });
-}
->>>>>>> 5e29e6ee1d2d2da502c53464852a9e67546efd7d
